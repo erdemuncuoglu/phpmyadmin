02d14d0b2c5c87cc5cac6ee99270e5b7eb552231
Erdem Uncuoğlu <erdem@mvc4.us>
2020-12-24 18:09:24 +0300

    Custom script to rebase and merge branches, run composer and yarn.

---
+++ my_build.conf.sh
@@ -0,0 +1,6 @@
+# Custom configuration file for `my_build`
+
+my_branches="ignore_5_0 base_5_0 delim_5_0 theme_5_0 index_5_0 rt_list_5_0 rt_editor_5_0 fix_fkcheck_5_0"
+my_stable="my_5_0"
+remote_repo="upstream"
+remote_branch="QA_5_0"
